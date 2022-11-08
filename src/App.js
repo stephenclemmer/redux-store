3f460d2ab6c59b80071fde21c3096dd60d9d16f7
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-11-07 20:47:55 -0800

    created the start of a paper card within a grid, that has a size of 3/12 per card.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+// import "./App.css";
 import Header from "./Components/Header";
 import Products from "./Components/Products";
+import Card from "./Components/Card/index.js";
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <>
-      <Header />
-      <Products />
+    <Container>
+      {/* <Header /> */}
+      <Grid container spacing={5}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Grid>
+      {/* <Products /> */}
+    </Container>
     </>
   );
 }
---
+++ src/Components/Card/index.js
@@ -0,0 +1,11 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Card = () => {
+  return(
+  <Grid item xs={3}>
+    <Paper elevation={3} square>Hello</Paper>
+  </Grid>
+  );
+};
+export default Card;
