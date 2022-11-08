16b38f140fa61c82ce552b20c9e74e44f4b83a8a
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-11-07 21:17:45 -0800

    card with image and two text fields

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,10 +1,29 @@
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import '../../App.css';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const Card = () => {
   return(
   <Grid item xs={3}>
-    <Paper elevation={3} square>Hello</Paper>
+    <Paper elevation={3}>
+      <img src="https://cdn.dribbble.com/users/5714894/screenshots/13446407/media/df77469f627f4858289de4fa461b65f9.jpg?compress=1&resize=1000x750&vertical=top" 
+      alt="" 
+      className="img"
+      />
+      <Box paddingX={1}>
+      <Typography variant="h6" component="h2">
+        Commodore 64
+      </Typography>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+      <Typography variant="body2" component="p">Hello World!</Typography>
+      </Box>
+    </Paper>
   </Grid>
   );
 };
