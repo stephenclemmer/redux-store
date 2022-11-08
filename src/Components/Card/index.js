a9311145f4e610cf5d0fdf2bd2d5b18f18558730
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-11-07 21:36:20 -0800

    just before rating is implemented

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -3,10 +3,12 @@ import Grid from '@mui/material/Grid';
 import '../../App.css';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import {} from '@mui/icons-material';
 
 const Card = () => {
   return(
-  <Grid item xs={3}>
+  <Grid item xs={4}>
     <Paper elevation={3}>
       <img src="https://cdn.dribbble.com/users/5714894/screenshots/13446407/media/df77469f627f4858289de4fa461b65f9.jpg?compress=1&resize=1000x750&vertical=top" 
       alt="" 
@@ -21,7 +23,13 @@ const Card = () => {
         display: 'flex',
         alignItems: 'center',
       }}>
-      <Typography variant="body2" component="p">Hello World!</Typography>
+      <Typography paddingX={1} variant="body2" component="p">Hello World!</Typography>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+      <Typography paddingX={1} variant="body2" component="p">Hello World!</Typography>
       </Box>
     </Paper>
   </Grid>
