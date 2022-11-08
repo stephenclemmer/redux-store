6c236988301b6f18b345c47cd6fc8c0fb9059a8b
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-11-08 00:20:20 -0800

    Navbar is up and styled

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import Products from "./Components/Products";
 import Card from "./Components/Card/index.js";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import SearchAppBar from "./Components/Appbar";
+// import MenuBar from "./Components/MenuBar";
 
 function App() {
   return (
     <>
-    <Container>
-      <Header />
-      <Grid container spacing={5}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </Grid>
-      {/* <Products /> */}
-    </Container>
+      <SearchAppBar sx={{marginY: 5}}/>
+      {/* <MenuBar /> */}
+      {/* <Container>
+      
+      </Container> */}
+      <Container>
+        {/* <Header /> */}
+        <Grid container spacing={5}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </Grid>
+        {/* <Products /> */}
+      </Container>
     </>
   );
 }
